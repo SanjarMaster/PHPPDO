f0b8e6b46ce82af145ecdfaf7638fc13b308b800
SanjarMaster <san_sxz@mail.ru>
2024-08-17 13:53:35 +0500

    ws

--- DBs/pgsql_connection.php
+++ DBs/pgsql_connection.php
@@ -1,13 +1,13 @@
 <?php
 
 declare(strict_types=1);
-
+  
 // рекомендуемые МНОЙ параметры
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_STRINGIFY_FETCHES => false,
     PDO::ATTR_EMULATE_PREPARES => false
-];
+];  
 
 //$options = [];
 
